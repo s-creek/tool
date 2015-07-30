bf83bb82be3ba194c4972a38e640f1a56d6cf924
s-creek <ogawa@oga-pc04.(none)>
2015-07-30 20:15:12 +0900

    update

--- memo.sh
+++ memo.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# borrowed from
+# http://d.hatena.ne.jp/tettou77-2/20100525/1274810510
+
 dir=`dirname $0`
 date=`date +%F_%T`
 memo=`zenity --entry --title="一言メモ" --text="メモを記録します" --width=400`
